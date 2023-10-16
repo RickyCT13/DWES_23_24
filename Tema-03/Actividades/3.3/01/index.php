754b5512dadf836014b228a07ebfbc8e1f021919
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-10-16 10:38:06 +0200

    Actividad 3.3 Realizada

---
+++ Tema-03/Actividades/3.3/01/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Crear un script PHP que permita mostrar los números del 1 al 100
+en una tabla a 10 filas y 10 columnas. Usar bucle while. */
+
+// Cargar vista del Índice
+include 'views/viewIndex.php';
+
+?>
---
+++ Tema-03/Actividades/3.3/02/models/modelTablaMult.php
@@ -0,0 +1,13 @@
+<?php 
+
+$numeros = [];
+$contador = 1;
+for ($i = 1; $i <= 10; $i++) {
+    $columnas = [];
+    for ($j = 1; $j <= 10; $j++) {
+        $columnas[] = $contador;
+        $contador++;
+    }
+}
+
+?>
