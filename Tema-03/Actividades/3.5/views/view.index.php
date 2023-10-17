0cbc984e4aa285cd838f4aad81922db9b3aa4096
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-10-17 10:39:36 +0200

    17/10/2023 Act 3.4 Realizada

---
+++ Tema-03/Actividades/3.5/views/view.index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php include 'layouts/head.html'?>
+  </head>
+  <body>
+    <!-- Capa Principal -->
+    <div class="container">
+      <header class="pb-3 mb-4 border-bottom">
+        <i class="bi bi-book"></i>
+        <span class="fs-3">Actividad 3.4 - Tabla de libros</span>
+      </header>
+      <legend>Tabla Libros</legend>
+      <table class="table">
+        <!-- Encabezado tabla -->
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>Género</th>
+                <th>Precio</th>
+            </tr>
+        </thead>
+        <!-- Cuerpo tabla -->
+        <tbody>
+            <?php foreach($libros as $libro): ?>
+                <!-- Se aplicará el mismo formato a cada campo de la tabla -->
+                <tr>
+                    <?php foreach($libro as $campo): ?>
+                        <td><?= $campo ?></td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        <!-- Pie de la tabla -->
+        <tfoot>
+            <!-- La celda ocupará tantas columnas (colspan)
+            como campos de cada registro haya (count($libro)) -->
+            <td colspan="<?= count($libro)?>">Nº Libros: <?= count($libros)?></td>
+        </tfoot>
+      </table>
+
+    </div>
+
+    <!-- Pie del documento -->
+    <?php include 'layouts/footer.html'?>
+
+    <!-- Bootstrap Javascript y popper -->
+    <?php include 'layouts/javascript.html'?>
+  </body>
+</html>
