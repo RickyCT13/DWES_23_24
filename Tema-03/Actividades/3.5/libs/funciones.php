e67c07be46f8c825cedf110455b0ecadbf408a5a
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-10-23 10:04:20 +0200

    23/10/2023

--- Tema-03/Actividades/3.5/libs/funciones.php
+++ Tema-03/Actividades/3.5/libs/funciones.php
@@ -64,29 +64,86 @@ function eliminar(&$tabla = [], $id_elemento) {
     return $tabla;
 }
 
-function ordenar($tabla = [], $campo) {
-    usort($tabla, function ($a, $b) use ($campo) {
-        return $a[$campo] <=> $b[$campo];
-    });
-    return $tabla;
-}
+/* 
 
-function actualizar(&$tabla = [], $valores = [], $indice) {
-    foreach ($valores as $i => $valor) {
-        if ($valor !== null && isset($tabla[$indice][$i])) {
-            $tabla[$indice][$i] = $valor;
+    Función: ordenar()
+    Descripción: Ordena los elementos de la tabla según uno de sus campos
+    Parámetros:
+        - Tabla
+        - Criterio de ordenación
+    Salida:
+        - Tabla ordenada
+
+*/
+function ordenar($tabla = [], $criterio) {
+    $campos = array_keys($tabla[0]);
+    foreach($campos as $campo) {
+        if ($criterio === $campo) {
+            usort($tabla, function ($a, $b) use ($campo) {
+                return $a[$campo] <=> $b[$campo];
+            });
         }
     }
     return $tabla;
 }
 
+
+/* 
+
+    Función: actualizar()
+    Descripción: Actualiza un registro de la tabla con los nuevos valores
+    Parámetros:
+        - Tabla
+        - Nuevos valores del registro (null si no se desea cambiar)
+        - Índice del registro que se desea actualizar
+    Salida:
+        - Tabla actualizada
+
+*/
+function actualizar(&$tabla = [], $valores = [], $indice) {
+    $indice--;
+    $campos = array_keys($tabla[0]);
+    foreach (array_combine($campos, $valores) as $campo => $valor) {
+        if ($valor !== null && isset($tabla[$indice][$campo])) {
+            $tabla[$indice][$campo] = $valor;
+        }
+    }
+    return $tabla;
+}
+
+/* 
+
+    Función: nuevo()
+    Descripción: Añade un nuevo registro a la tabla
+    Parámetros:
+        - Tabla
+        - Valores del registro (si el 1er elemento, correspondiente al id, es nulo,
+        se asignará el número de registros + 1)
+    Salida:
+        - Tabla actualizada
+
+*/
 function nuevo(&$tabla = [], $valores) {
+    if ($valores[0] === null) {
+        $valores[0] = count($tabla) + 1;
+    }
     $campos = array_keys($tabla[0]);
     $nuevoRegistro = array_combine($campos, $valores);
     array_push($tabla, $nuevoRegistro);
     return $tabla;
 }
 
+/* 
+
+    Función: filtrar()
+    Descripción: Filtrar los registros de una tabla a partir de una expresión de búsqueda
+    Parámetros:
+        - Tabla
+        - Expresión de búsqueda
+    Salida:
+        - Tabla filtrada
+
+*/
 function filtrar(&$tabla = [], $expresionBusqueda) {
     $tablaFiltrada = array_filter($tabla, function($registro) use ($expresionBusqueda) {
         return in_array($expresionBusqueda, $registro);
@@ -96,22 +153,29 @@ function filtrar(&$tabla = [], $expresionBusqueda) {
 
 $libros = generarTabla();
 
-print_r($libros);
+// Pruebas para el correcto funcionamiento:
 
-eliminar($libros, 4);
+// echo "Tabla libros:\n";
+// print_r($libros);
 
-print_r($libros);
+// echo "Eliminar el 4to ejemplar:\n";
+// eliminar($libros, 4);
 
-$librosOrdenados = ordenar($libros, $libros[0]['autor']);
+// print_r($libros);
 
-print_r($librosOrdenados);
+// echo "Ordenar libros por autor:\n";
+// $librosOrdenados = ordenar($libros, 'autor');
 
-actualizar($libros, [null, null, null, null, 13.99], 0);
+// print_r($librosOrdenados);
 
-print_r($libros);
+// echo "Actualizar el precio del primer ejemplar:\n";
+// actualizar($libros, [null, null, null, null, 13.99], 1);
 
-$libros = nuevo($libros, [5, "1984", "George Orwell", "Distopia", 15.95]);
+// print_r($libros);
 
-print_r($libros);
+// echo "Añadir un nuevo libro:\n";
+// $libros = nuevo($libros, [null, "1984", "George Orwell", "Distopia", 15.95]);
+
+// print_r($libros);
 
 ?>
