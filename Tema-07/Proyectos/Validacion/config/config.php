25a40b40555e35e93ef32af8caf4bd932472ec93
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-01-12 14:55:03 +0100

    12/1/2023 Avances proyecto validación

---
+++ Tema-07/Proyectos/Validacion/config/config.php
@@ -0,0 +1,16 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES_23_24/Tema-07/Proyectos/Validacion/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
---
+++ Tema-07/Proyectos/Validacion/template/partials/error.php
@@ -0,0 +1,8 @@
+<?php if (isset($this->error)) : ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Mensaje: </strong> <?= $this->mensaje; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                </button>
+        </div>
+<?php endif; ?>
