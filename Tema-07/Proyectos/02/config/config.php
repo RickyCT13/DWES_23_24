ed58b29aff162d8de29424ed9853ba08bc564ff1
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-01-22 18:24:17 +0100

    22/1/2024 -

---
+++ Tema-07/Proyectos/02/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/DWES_23_24/Tema-07/Proyectos/02/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+?>
