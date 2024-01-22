88afa06afdaafe3f3c013a079e0b14987dde21d1
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-01-22 16:53:45 +0100

    22/01/2024 Proyecto 7.1 entregado

--- Tema-07/Proyectos/01/template/partials/header.php
+++ Tema-07/Proyectos/01/template/partials/header.php
@@ -4,7 +4,7 @@
         <div class="jumbotron jumbotron-fluid">
         <div class="container">
             <h3 class="display-7">Proyecto Gesbank </h3>
-            <p class="lead">Tema 6 - DWES - Curso 23/24</p>
+            <p class="lead">Tema 7 - DWES - Curso 23/24</p>
         </div>
         </div>
     </hgroup>
--- Tema-07/Proyectos/01/template/partials/menu.php
+++ Tema-07/Proyectos/01/template/partials/menu.php
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?= URL ?>cuenta">Cuentas  </a>
+          <a class="nav-link active" aria-current="page" href="<?= URL ?>cuenta">Cuentas</a>
         </li>
         <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="<?= URL ?>cliente">Clientes</a>
