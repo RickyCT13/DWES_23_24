d533f998a3eefda8fd62bd4b80a1de5a3a343256
RickyCT13 <ricardomorenocantea13@gmail.com>
2024-03-11 14:57:18 +0100

    11/3/2024 - (Arreglar email y realizar acts Laravel)

--- Tema-12/Actividades/03/app/Http/Controllers/AccountController.php
+++ Tema-12/Actividades/03/app/Http/Controllers/AccountController.php
@@ -5,61 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AccountController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+class AccountController extends Controller {
+    public function index() {
+        return "Home - Cuentas";
+    }
+    public function create() {
+        return "Creando cuenta";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function read($id) {
+        return "Datos de la cuenta<br>Id: $id";
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update($id) {
+        return "Actualizando cuenta con id $id";
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    public function delete($id) {
+        return "Eliminando cuenta con id $id";
     }
 }
